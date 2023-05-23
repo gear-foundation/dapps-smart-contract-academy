0160dae55a44381f90b06a2bd1abd299e8da9d4f
ereburg <gimmethenick@gmail.com>
2023-05-23 13:40:42 +0300

    upd copy

--- frontend/src/components/layout/footer/footer.tsx
+++ frontend/src/components/layout/footer/footer.tsx
@@ -24,7 +24,7 @@ export const Footer = () => (
       ))}
     </ul>
     <p className="text-white text-opacity-60 text-xs">
-      &copy; {new Date().getFullYear()} Gear Technologies, Inc. All Rights
+      &copy; {new Date().getFullYear()} Gear Foundation, Inc. All Rights
       Reserved.
     </p>
   </footer>
