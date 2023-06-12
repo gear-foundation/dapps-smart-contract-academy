0b4a159e496de51d6d35027e9201a0b116393668
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-06-12 19:04:40 +0400

    Fix tests

--- contracts/6-program-factory/escrow/tests/escrow_test.rs
+++ contracts/6-program-factory/escrow/tests/escrow_test.rs
@@ -6,12 +6,13 @@ const BUYER: u64 = 100;
 const SELLER: u64 = 101;
 const PRICE: u128 = 100_000;
 const ESCROW_ID: u64 = 1;
+const FACTORY_ID: u64 = 10;
 
 fn init_escrow(sys: &System) {
     sys.init_logger();
     let escrow = Program::current(sys);
     let res = escrow.send(
-        BUYER,
+        FACTORY_ID,
         InitEscrow {
             seller: SELLER.into(),
             buyer: BUYER.into(),
@@ -20,7 +21,7 @@ fn init_escrow(sys: &System) {
     );
     let log = Log::builder()
         .source(ESCROW_ID)
-        .dest(BUYER)
+        .dest(FACTORY_ID)
         .payload(EscrowEvent::ProgramInitialized);
     assert!(res.contains(&log));
 }
@@ -32,11 +33,11 @@ fn deposit() {
 
     let escrow = sys.get_program(ESCROW_ID);
 
-    sys.mint_to(BUYER, PRICE);
+    sys.mint_to(FACTORY_ID, PRICE);
 
-    let res = escrow.send_with_value(BUYER, EscrowAction::Deposit(BUYER.into()), PRICE);
+    let res = escrow.send_with_value(FACTORY_ID, EscrowAction::Deposit(BUYER.into()), PRICE);
     let log = Log::builder()
-        .dest(BUYER)
+        .dest(FACTORY_ID)
         .payload(EscrowEvent::FundsDeposited);
     assert!(res.contains(&log));
 
@@ -51,20 +52,20 @@ fn deposit_failures() {
 
     let escrow = sys.get_program(ESCROW_ID);
 
-    sys.mint_to(BUYER, 2 * PRICE);
+    sys.mint_to(FACTORY_ID, 2 * PRICE);
     // must fail since BUYER attaches not enough value
-    let res = escrow.send_with_value(BUYER, EscrowAction::Deposit(BUYER.into()), 2 * PRICE - 500);
+    let res = escrow.send_with_value(FACTORY_ID, EscrowAction::Deposit(BUYER.into()), 2 * PRICE - 500);
     assert!(res.main_failed());
 
     // must fail since the message sender is not BUYER
-    let res = escrow.send(SELLER, EscrowAction::Deposit(BUYER.into()));
+    let res = escrow.send(FACTORY_ID, EscrowAction::Deposit(SELLER.into()));
     assert!(res.main_failed());
 
     // successful deposit
-    let res = escrow.send_with_value(BUYER, EscrowAction::Deposit(BUYER.into()), PRICE);
+    let res = escrow.send_with_value(FACTORY_ID, EscrowAction::Deposit(BUYER.into()), PRICE);
     assert!(!res.main_failed());
 
     // must fail since the state must be `AwaitingPayment`
-    let res = escrow.send_with_value(BUYER, EscrowAction::Deposit(BUYER.into()), PRICE);
+    let res = escrow.send_with_value(FACTORY_ID, EscrowAction::Deposit(BUYER.into()), PRICE);
     assert!(res.main_failed());
 }
